ebd4a5908591ba6c2f2dd489050746a05afd685a
MudarGit <kneehighmud@gmail.com>
2015-05-01 10:00:44 -0400

    + Messages Model and Controller

---
+++ GoldTeamProject7/Controllers/MessagesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GoldTeamProject7.Models;
+
+namespace GoldTeamProject7.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Messages
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Messages.ToListAsync());
+        }
+
+        // GET: Messages/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Messages messages = await db.Messages.FindAsync(id);
+            if (messages == null)
+            {
+                return HttpNotFound();
+            }
+            return View(messages);
+        }
+
+        // GET: Messages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Messages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,Message,DateSent,TransactionID")] Messages messages)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Messages.Add(messages);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(messages);
+        }
+
+        // GET: Messages/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Messages messages = await db.Messages.FindAsync(id);
+            if (messages == null)
+            {
+                return HttpNotFound();
+            }
+            return View(messages);
+        }
+
+        // POST: Messages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,Message,DateSent,TransactionID")] Messages messages)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(messages).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(messages);
+        }
+
+        // GET: Messages/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Messages messages = await db.Messages.FindAsync(id);
+            if (messages == null)
+            {
+                return HttpNotFound();
+            }
+            return View(messages);
+        }
+
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Messages messages = await db.Messages.FindAsync(id);
+            db.Messages.Remove(messages);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GoldTeamProject7/Models/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoldTeamProject7.Models
+{
+    public class Messages
+    {
+        public int ID { get; set; }        
+        public string Message { get; set; }
+        public DateTime DateSent { get; set; }
+        public int TransactionID { get; set; }
+
+    }
+}
