18343d105f46756e1acc87770930f69c884d0677
Seth Johnson <sethjdesigns@gmail.com>
2015-05-01 20:21:31 -0400

    merge cleanup 2

---
+++ GoldTeamProject7/Migrations/201505020020297_fridaymerch2.cs
@@ -0,0 +1,28 @@
+namespace GoldTeamProject7.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fridaymerch2 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Messages",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Message = c.String(),
+                        DateSent = c.DateTime(),
+                        TransactionID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Messages");
+        }
+    }
+}
