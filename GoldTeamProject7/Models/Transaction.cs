22a7c056fbc3cfcc55b5015aaf8753ad3ed9a8d9
MudarGit <kneehighmud@gmail.com>
2015-05-04 10:41:59 -0400

    + Transaction Class

---
+++ GoldTeamProject7/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoldTeamProject7.Models
+{
+    public class Transaction
+    {
+        public int ID { get; set; }
+        public int MessageID { get; set; }
+        public DateTime? Created { get; set; }
+        public int BuyerID { get; set; }
+        public int ProductID { get; set; }
+
+    }
+}
