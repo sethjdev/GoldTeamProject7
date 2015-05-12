3a8243749cacf11f928b5109be2a659a762901ce
Seth Johnson <sethjdesigns@gmail.com>
2015-05-11 22:16:02 -0400

    updated home view and added Masonry.js to display tile products

--- GoldTeamProject7/Controllers/ProductsController.cs
+++ GoldTeamProject7/Controllers/ProductsController.cs
@@ -60,7 +60,7 @@ namespace GoldTeamProject7.Controllers
 
             return View(product);
         }
-
+        [Authorize]
         // GET: Products/Create
         public ActionResult Create()
         {
